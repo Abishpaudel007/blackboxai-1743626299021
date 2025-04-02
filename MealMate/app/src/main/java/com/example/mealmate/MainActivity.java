af1d90f6ff5bdd9c64afc52b7159d2efc4ad1e62
Abishpaudel007 <136405684+Abishpaudel007@users.noreply.github.com>
2025-04-03 02:23:59 +0545

    Add MealMate/app/src/main/java/com/example/mealmate/MainActivity.java 1743626339997

---
+++ MealMate/app/src/main/java/com/example/mealmate/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.mealmate;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.nav_meals) {
+                selectedFragment = new MealsFragment();
+            } else if (itemId == R.id.nav_grocery) {
+                selectedFragment = new GroceryFragment();
+            } else if (itemId == R.id.nav_profile) {
+                selectedFragment = new ProfileFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+                return true;
+            }
+            return false;
+        });
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            bottomNav.setSelectedItemId(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
